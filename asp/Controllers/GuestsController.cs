85c11b72fc23d4de6db5f0f9f10f954a30cf533f
Mike <Mike@DESKTOP-TU8RGUM>
2016-05-25 00:06:11 +0800

    更新

---
+++ asp/Controllers/GuestsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using asp.Models;
+
+namespace asp.Controllers
+{
+    public class GuestsController : Controller
+    {
+        private aspContext db = new aspContext();
+
+        // GET: Guests
+        public ActionResult Index()
+        {
+            return View(db.Guests.ToList());
+        }
+
+        // GET: Guests/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guest guest = db.Guests.Find(id);
+            if (guest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guest);
+        }
+
+        // GET: Guests/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Guests/Create
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "uid,email,name")] Guest guest)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Guests.Add(guest);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(guest);
+        }
+
+        // GET: Guests/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guest guest = db.Guests.Find(id);
+            if (guest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guest);
+        }
+
+        // POST: Guests/Edit/5
+        // 为了防止“过多发布”攻击，请启用要绑定到的特定属性，有关 
+        // 详细信息，请参阅 http://go.microsoft.com/fwlink/?LinkId=317598。
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "uid,email,name")] Guest guest)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(guest).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(guest);
+        }
+
+        // GET: Guests/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Guest guest = db.Guests.Find(id);
+            if (guest == null)
+            {
+                return HttpNotFound();
+            }
+            return View(guest);
+        }
+
+        // POST: Guests/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Guest guest = db.Guests.Find(id);
+            db.Guests.Remove(guest);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ asp/Models/Guest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace asp.Models
+{
+    public class Guest
+    {
+        [Key]
+        public int uid { get; set;  } 
+        public string email { get; set; }
+        public string name { get; set; }
+        protected string pwd { get; set; }
+        protected string logip { get; set; }
+        protected DateTime logtime { get; set; }
+    }
+}
